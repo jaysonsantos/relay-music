1fc348402d5847c9a41d15fed569199897327547
Jayson Reis <santosdosreis@gmail.com>
2020-11-08 23:30:35 +0100

    Make the note playing genric so it can run by itself on the main loop

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 
 use cortex_m_rt::entry;
 
-use nb::block;
 use stm32f1xx_hal::{pac, prelude::*, time::Hertz, timer::Timer};
 
-use defmt::info;
+use defmt::{info, Format};
 use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::timer::CountDown;
 
 #[defmt::timestamp]
 fn timestamp() -> u64 {
@@ -37,6 +37,74 @@ lazy_static::lazy_static! {
     static ref F: Hertz = 350.hz();
     static ref G: Hertz = 393.hz();
 }
+#[derive(Format)]
+enum NotePlayerStatus {
+    Start,
+    Up,
+    Down,
+}
+
+struct NotePlayer<'a, O, C>
+where
+    O: OutputPin,
+    C: CountDown,
+{
+    notes: &'a [&'static Hertz],
+    pin: O,
+    timer: C,
+    current_note_idx: usize,
+    status: NotePlayerStatus,
+}
+
+impl<'a, O, C> NotePlayer<'a, O, C>
+where
+    O: OutputPin,
+    C: CountDown,
+{
+    pub fn new(notes: &'a [&'static Hertz], pin: O, timer: C) -> Self {
+        Self {
+            notes,
+            pin,
+            timer,
+            current_note_idx: 0,
+            status: NotePlayerStatus::Start,
+        }
+    }
+
+    pub fn tick(&mut self)
+    where
+        <C as CountDown>::Time: From<Hertz>,
+    {
+        match self.status {
+            NotePlayerStatus::Start => {
+                self.timer.start::<Hertz>(*self.get_current_note());
+                let _ = self.pin.set_high();
+                self.status = NotePlayerStatus::Up;
+            }
+            NotePlayerStatus::Up => {
+                if self.timer.wait().is_ok() {
+                    let _ = self.pin.set_low();
+                    self.status = NotePlayerStatus::Down;
+                }
+            }
+            NotePlayerStatus::Down => {
+                if self.timer.wait().is_ok() {
+                    let _ = self.pin.set_high();
+                    self.status = NotePlayerStatus::Up;
+                }
+            }
+        }
+    }
+
+    fn get_current_note(&self) -> &'a Hertz {
+        unsafe { self.notes.get_unchecked(self.current_note_idx) }
+    }
+
+    fn increment_note(&mut self) {
+        self.current_note_idx = (self.current_note_idx + 1) % self.notes.len();
+        self.status = NotePlayerStatus::Start;
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -60,26 +128,26 @@ fn main() -> ! {
 
     // Configure gpio B pin 12 as a push-pull output. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut relay = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+    let relay = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
 
     // Configure the syst timer to trigger an update every second
-    let mut note_timer = Timer::tim1(dp.TIM1, &clocks, &mut rcc.apb2).start_count_down(1.hz());
+    let note_timer = Timer::tim1(dp.TIM1, &clocks, &mut rcc.apb2).start_count_down(1.hz());
     let mut duration_timer = Timer::syst(cp.SYST, &clocks).start_count_down(2.hz());
 
-    let notes: [&Hertz; 16] = [
+    let notes: &[&Hertz] = &[
         &E, &E, &F, &G, &G, &F, &E, &D, &C, &C, &D, &E, &D, &D, &C, &C,
     ];
+    let mut notes_player = NotePlayer::new(notes, relay, note_timer);
 
     loop {
-        for note in notes.iter() {
-            info!("Playing {:u32}", note.0);
-            note_timer.start(**note);
-            while let Err(nb::Error::WouldBlock) = duration_timer.wait() {
-                block!(note_timer.wait()).unwrap();
-                relay.set_high().unwrap();
-                block!(note_timer.wait()).unwrap();
-                relay.set_low().unwrap();
-            }
+        info!(
+            "Playing {:u32} {:?}",
+            notes_player.get_current_note().0,
+            notes_player.status
+        );
+        while let Err(nb::Error::WouldBlock) = duration_timer.wait() {
+            notes_player.tick();
         }
+        notes_player.increment_note();
     }
 }
